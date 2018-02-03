7ee6ed77f9e54f18fc51ace2b34b3357b74b67ab
yanting <yanting.liang@gmail.com>
2018-02-03 02:00:57 -0600

    ios11 version
    This app only works for ios11 so far

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,41 @@
+
+//  ViewController.swift
+//  Xylophone
+
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    var audioPlayer: AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        playSound(soundFileName: soundArray[sender.tag - 1])
+    
+    }
+    
+    func playSound(soundFileName : String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch  {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
+        
+    
+  
+
+}
+
